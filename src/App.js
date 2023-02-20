35b37f8dec022d74871e1f86da766df4d9a97a76
Visishta-ch <Visishtachatla@gmail.com>
2023-02-20 12:38:05 +0530

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
+import React, { Suspense } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Module from './components/Slides/Module';
 import Home from './components/Home';
 import SearchSlide from './components/ModuleSection/SearchSlide';
-import Module4 from './components/Slides/Module4';
-import Module5 from './components/Slides/Module5';
-import Module3 from './components/Slides/Module3';
-import Module2 from './components/Slides/Module2';
 import ModuleTour from './components/ModuleTour';
-
+const Module = React.lazy(() => import('./components/Slides/Module'));
+const Module2 = React.lazy(() => import('./components/Slides/Module2'));
+const Module3 = React.lazy(() => import('./components/Slides/Module3'));
+const Module4 = React.lazy(() => import('./components/Slides/Module4'));
+const Module5 = React.lazy(() => import('./components/Slides/Module5'));
 
 function App() {
   return (
-    <Switch>
-      <Route path="/" exact>
-    
-        <Home />
-      </Route>
-      <Route path='/module'>
-        <ModuleTour />
-      </Route>
-      <Route path="/module1">
-        <Module />
-      </Route>
-      <Route path="/module2">
-        <Module2 />
-      </Route>
-      <Route path="/module3">
-        <Module3 />
-      </Route>
-      <Route path="/module4">
-        <Module4 />
-      </Route>
-      <Route path="/module5">
-        <Module5 />
-      </Route>
+    <>
+      <Suspense>
+        <Switch>
+          <Route path="/" exact>
+            <Home />
+          </Route>
+          <Route path="/module">
+            <ModuleTour />
+          </Route>
+          <Route path="/module1">
+            <Module />
+          </Route>
+          <Route path="/module2">
+            <Module2 />
+          </Route>
+          <Route path="/module3">
+            <Module3 />
+          </Route>
+          <Route path="/module4">
+            <Module4 />
+          </Route>
+          <Route path="/module5">
+            <Module5 />
+          </Route>
 
-      <Route path="/searchpage">
-        <SearchSlide />
-      </Route>
-    </Switch>
+          <Route path="/searchpage">
+            <SearchSlide />
+          </Route>
+        </Switch>
+      </Suspense>
+    </>
   );
 }
 
